d05a092d2aab6d4da8b95182ecbf53f467b99486
JojoMoiEtre <jonathan.joseph345@gmail.com>
2022-08-27 10:04:17 +0200

    <bot et dmChelou

--- Events/ready.js
+++ Events/ready.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js")
 
 module.exports = async bot => {
+
+    bot.user.setActivity(`la plèbe (${config.prefix}help)`, {type: 3}) 
     
     console.log(`\n> ${bot.user.tag} est en ligne !`)
 }
