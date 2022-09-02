d94b540d20c6dd9a2a839d98edfd6c4676647d26
JojoMoiEtre <79185789+JojoMoiEtre@users.noreply.github.com>
2022-09-02 07:03:41 +0200

    Update verifAnnivIsTime.js

--- Programmes/verifAnnivIsTime.js
+++ Programmes/verifAnnivIsTime.js
@@ -31,14 +31,12 @@ module.exports = async bot => {
                 let dateJour = dateDuJour();
                 let heure = date.getHours();
                 let minute = date.getMinutes();
-                
-                console.log(heure)
 
                 if(heure === 5) {
                     if(minute === 10) {
                         db.query(`SELECT * FROM \`${guild.id}\` WHERE DateJourMois = \'${dateJour}\'`, async (err, req) => {
 
-                            if(err) throw err;
+                            if(err) return console.log(erreur);
                             if(req.length > 0) {
 
                                 let command = require(`./annivIsTime`)
